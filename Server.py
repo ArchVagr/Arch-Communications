ecc18e3d87dc353fcd7c16a2d3c360becfccb7e9
ARCH <tagahaha86@gmail.com>
2025-09-26 01:35:42 +0200

    Applied Pydantic for clients requests verification and SQlAlchemy

--- Server.py
+++ Server.py
@@ -1,27 +1,38 @@
+import pydantic
 from flask import Flask,request
 import sqlite3 as sql
 import main
-
+from pydantic import BaseModel,constr
 
 #SERVER
 
 
 server=Flask(__name__)
 
+class User(BaseModel):
+    username:str
+    password:constr(min_length=8)
+
 @server.post("/add")
 def handle_signup():
-    query = request.get_json()
+    query = User(request.get_json())
     try:
-        main.add_user(query["username"],query["password"])
-        return {"status":True,id:main.id_return(query["username"],query["password"])}
+        main.add_user(query.username,query.password)
+        return {"status":True,id:main.id_return(query.username,query.password)}
     except sql.IntegrityError:
         return  {"status":False}
+    else:
+        return {"status": False}
 
 @server.post("/signin")
 def signin():
-    query=request.get_json()
-    check=main.id_return(query["username"],query["password"])
-    return {"status":check}
+    query=User(request.get_json())
+    try:
+        check=main.id_return(query.username,query.password)
+        return {"status":check}
+    except pydantic.ValidationError:
+        return {"status":False}
+
 
 @server.post("/search")
 def search():
--- main.py
+++ main.py
@@ -1,100 +1,137 @@
-import json
-import os.path
-import sqlite3 as sql
 from PATH import PATH
+from sqlalchemy import create_engine,String,Integer,ForeignKey,Column,DateTime,text
+from sqlalchemy.orm import sessionmaker,declarative_base,Session,relationship
 
-def create_table_messages():
+engine = create_engine(PATH)
+session = sessionmaker(bind=engine)
 
-    connection = sql.connect(PATH)
-    connection.execute("PRAGMA foreign_keys = ON;")  # включаем поддержку внешних ключей
-    cursor = connection.cursor()
+Base=declarative_base()
 
-    cursor.execute(
-        """
-        CREATE TABLE IF NOT EXISTS Accounts (
-            user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            username INTEGER NOT NULL,
-            password INTEGER NOT NULL
-        );
-        """
-    )
+class User(Base):
+    __tablename__ = "users"
 
-    connection.commit()
-    cursor.close()
-    connection.close()
+    id=Column(Integer,primary_key=True,autoincrement=True)
+    username=Column(String(18),nullable=False)
+    password=Column(String,unique=True,nullable=False)
 
-def add_user(username,password):
+    messages = relationship("Message", back_populates="author")
 
 
-    connection = sql.connect(PATH)
+class Chat(Base):
+    __tablename__='chats'
 
-    cursor = connection.cursor()
-    cursor.execute("""
-    INSERT INTO Accounts (username,password)
-    VALUES
-    (?,?)""",(username,password))
-    connection.commit()
-    cursor.close()
+    chat_id=Column(Integer,primary_key=True,autoincrement=True)
+    participant_1 = Column(Integer, ForeignKey("users.id"))
+    participant_2 = Column(String(18), nullable=False)
 
-def verify_user(username,password):
+    messages = relationship('Messages', back_populates='chat')
 
 
-    connection = sql.connect(PATH)
+class Messages(Base):
+    __tablename__='messages'
 
-    cursor=connection.cursor()
-    cursor.execute("""
-    SELECT * FROM Accounts WHERE username=? and password=?
-    """,(username,password))
-    response=cursor.fetchall()
-    if response:
-        return list(response)
-    else:
-        return None
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    chat=Column(Integer,ForeignKey('chats.chat_id'),nullable=False)
+    author=Column(Integer,ForeignKey('users.id'),nullable=False)
+    created_at = Column(DateTime, server_default=text("CURRENT_TIMESTAMP"))
 
-def user_search(username):
+    author=relationship('User',back_populates='messages')
+    chat=relationship('Chat',back_populates='messages')
 
+Base.metadata.create_all(engine)
 
-    connection = sql.connect(PATH)
-    cursor = connection.cursor()
-    cursor.execute("""
-        SELECT username,id FROM Accounts WHERE username LIKE ?
-        """, (username + "%",))
-    response = cursor.fetchall()
-    return response if response else False
-
-def id_return(username,password):
-
-
-    connection = sql.connect(PATH)
-    cursor = connection.cursor()
-    cursor.execute("""
-        SELECT id FROM Accounts WHERE username = ? AND password=?
-        """, (username,password))
-    response = cursor.fetchone()
-    if response:
-        for i in response:
-            return i
-    else:
-        return False
-
-def create_chat(participant_1,participant_2):
-
-
-    connection = sql.connect(PATH)
-    cursor = connection.cursor()
-    cursor.execute('INSERT INTO Chats(participant_1,participant_2) VALUES(?,?)',
-                   (participant_1,participant_2))
-    connection.commit()
-    cursor.close()
-
-def create_message(chat_id,sender,text):
-
-    connection = sql.connect(PATH)
-
-    cursor = connection.cursor()
-    cursor.execute('''INSERT INTO Messages(sender_id,chat_id,message_text) VALUES(?,?,?)''',(chat_id,sender,text))
-
-    connection.commit()
-    cursor.close()
-
-create_table_messages()
+# def create_table_messages():
+#
+#     connection = sql.connect(PATH)
+#     connection.execute("PRAGMA foreign_keys = ON;")  # включаем поддержку внешних ключей
+#     cursor = connection.cursor()
+#
+#     cursor.execute(
+#         """
+#         CREATE TABLE IF NOT EXISTS Accounts (
+#             user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+#             username INTEGER NOT NULL,
+#             password INTEGER NOT NULL
+#         );
+#         """
+#     )
+#
+#     connection.commit()
+#     cursor.close()
+#     connection.close()
+#
+# def add_user(username,password):
+#
+#
+#     connection = sql.connect(PATH)
+#
+#     cursor = connection.cursor()
+#     cursor.execute("""
+#     INSERT INTO Accounts (username,password)
+#     VALUES
+#     (?,?)""",(username,password))
+#     connection.commit()
+#     cursor.close()
+#
+# def verify_user(username,password):
+#
+#
+#     connection = sql.connect(PATH)
+#
+#     cursor=connection.cursor()
+#     cursor.execute("""
+#     SELECT * FROM Accounts WHERE username=? and password=?
+#     """,(username,password))
+#     response=cursor.fetchall()
+#     if response:
+#         return list(response)
+#     else:
+#         return None
+#
+# def user_search(username):
+#
+#
+#     connection = sql.connect(PATH)
+#     cursor = connection.cursor()
+#     cursor.execute("""
+#         SELECT username,id FROM Accounts WHERE username LIKE ?
+#         """, (username + "%",))
+#     response = cursor.fetchall()
+#     return response if response else False
+#
+# def id_return(username,password):
+#
+#
+#     connection = sql.connect(PATH)
+#     cursor = connection.cursor()
+#     cursor.execute("""
+#         SELECT id FROM Accounts WHERE username = ? AND password=?
+#         """, (username,password))
+#     response = cursor.fetchone()
+#     if response:
+#         for i in response:
+#             return i
+#     else:
+#         return False
+#
+# def create_chat(participant_1,participant_2):
+#
+#
+#     connection = sql.connect(PATH)
+#     cursor = connection.cursor()
+#     cursor.execute('INSERT INTO Chats(participant_1,participant_2) VALUES(?,?)',
+#                    (participant_1,participant_2))
+#     connection.commit()
+#     cursor.close()
+#
+# def create_message(chat_id,sender,text):
+#
+#     connection = sql.connect(PATH)
+#
+#     cursor = connection.cursor()
+#     cursor.execute('''INSERT INTO Messages(sender_id,chat_id,message_text) VALUES(?,?,?)''',(chat_id,sender,text))
+#
+#     connection.commit()
+#     cursor.close()
+#
+# create_table_messages()
